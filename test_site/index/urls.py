1ce619d8f0f279d02af9732bdca479452e276108
= <=>
2025-03-14 14:12:15 +0000

    [UPDATE] index

--- test_site/index/urls.py
+++ test_site/index/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import index
 
-urlpatterns = [
+app_name="index"
 
+urlpatterns = [
     path('', index, name="index")
 ]
--- test_site/index/views.py
+++ test_site/index/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+from django.conf import settings
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse('i adore you BO')
+
+    print('------',settings.TEMPLATES)
+    context = {
+        "title": "Главная страница",
+        'user1': "Boris Isac",
+        'user2': "Oxana Bruieva",
+    }
+    return HttpResponse(f"{context['user1']}-{context['user2']}")
+    #return render(request, "templates\index\index.html", context=context)
